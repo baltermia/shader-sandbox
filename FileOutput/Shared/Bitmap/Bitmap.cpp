c1f0317fad886bcd9ec75cd14caf0a5295eb1234
baltermia <54800864+baltermia@users.noreply.github.com>
2023-06-22 22:00:43 +0200

    Fully implement Bitmap classes

--- FileOutput/Shared/Bitmap/Bitmap.cpp
+++ FileOutput/Shared/Bitmap/Bitmap.cpp
@@ -1,14 +1,64 @@
 #include "Bitmap.h"
-#include "Header.cpp"
+#include <fstream>
 
-Bitmap::Bitmap(std::string path, const char* data)
+Bitmap::Bitmap(std::string path, uint32_t width, uint32_t height, char* data, uint16_t depth)
 	: m_path(path),
 	  m_data(data),
+	  m_header(new BitmapHeader(width, height, depth))
+{ }
+
+Bitmap::Bitmap(std::string path)
+	: m_path(path),
+	  m_data(nullptr),
 	  m_header(nullptr)
 { }
 
 Bitmap::~Bitmap()
 {
 	delete[] m_data;
-	delete[] m_header;
+	delete m_header;
+}
+
+void Bitmap::setData(char* data)
+{
+	m_data = data;
+}
+
+const char* Bitmap::data() const
+{
+	return m_data;
+}
+
+const BitmapHeader* Bitmap::header() const
+{
+	return m_header;
+}
+
+bool Bitmap::read()
+{
+	std::ifstream file(m_path);
+
+	// Seek to end of file.
+	file.seekg(0, std::ios_base::end);
+
+	// get file-size
+	size_t size = file.tellg();
+	size_t header_size = sizeof(BitmapHeader);
+	size_t data_size = size - header_size;
+
+	// go to beginning
+	file.seekg(0);
+
+	// read header data
+	char* header_data = new char[header_size];
+	file.read(header_data, header_size);
+	delete m_header;
+	m_header = reinterpret_cast<BitmapHeader*>(header_data);
+
+	// read content data
+	delete m_data;
+	m_data = new char[data_size];
+	file.read(m_data, data_size);
+
+	file.close();
 }
--- FileOutput/Shared/Bitmap/Bitmap.h
+++ FileOutput/Shared/Bitmap/Bitmap.h
@@ -1,17 +1,19 @@
 #pragma once
 
-#include "..\pch.h"
-
+// std
 #include <string>
 #include <vector>
+
+// bmp
+#include "Header.hpp"
 
 class Bitmap
 {
 	static const std::string s_extension;
 
 	std::string m_path;
-	const char* m_data;
-	char* m_header;
+	char* m_data;
+	BitmapHeader m_header;
 
 public:
 	/// <summary>
@@ -19,14 +21,15 @@ public:
 	/// </summary>
 	/// <param name="path">The path in the file-system</param>
 	/// <param name="data">Optional data that will be written as the bitmaps conent</param>
-	Bitmap(std::string path, const char* data = nullptr);
+	Bitmap(std::string path, uint32_t width, uint32_t height, char* data, uint16_t depth = 24);
+	Bitmap(std::string path);
 
 	~Bitmap();
 
 	/// <summary>
 	/// Sets the content-data. This does not write the data to the file system.
 	/// </summary>
-	void setData(const char* data);
+	void setData(char* data);
 	/// <summary>
 	/// The content-data of the bitmap file
 	/// </summary>
@@ -35,7 +38,7 @@ public:
 	/// <summary>
 	/// The header data of the bitmap file, generated from the given data
 	/// </summary>
-	const char* header() const;
+	const BitmapHeader& header() const;
 
 	/// <summary>
 	/// Tries to read data from the given path
@@ -46,7 +49,7 @@ public:
 	/// <summary>
 	/// Tries to write the bitmap data to the given path
 	/// </summary>
-	bool write();
+	bool write() const;
 
 	/// <summary>
 	/// Releases any data, same as if the destructor would be called. The Bitmap-object cannot be used anymore
