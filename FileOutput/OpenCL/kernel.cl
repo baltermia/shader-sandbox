a1be82d54dcddc72df6ffc5affe0a46158f805fa
baltermia <54800864+baltermia@users.noreply.github.com>
2023-06-23 15:51:39 +0200

    Try to work with 2d effect

--- FileOutput/OpenCL/kernel.cl
+++ FileOutput/OpenCL/kernel.cl
@@ -1,10 +1,13 @@
-__kernel void apply_shader(__global unsigned  char* data)
+__kernel void apply_shader(__global unsigned char* data,
+						   __constant int* width,
+						   __constant int* height,
+						   __constant int* channels)
 {
-	int i = get_global_id(0);
+	int x = get_global_id(0);
+	int y = get_global_id(1);
 
-	__global unsigned char* curr = &data[i * 3];
+	int i = (x + y * *height) * *channels;
 
-	*curr = convert_char(0xAAAAAA);
-	*++curr = convert_char(0xAAAAAA);
-	*++curr = convert_char(0xAAAAAA);
+	for (int j = 0; j < *channels; ++j)
+		data[i + j] = convert_char(0xAA);
 }
--- FileOutput/OpenCL/main.cpp
+++ FileOutput/OpenCL/main.cpp
@@ -9,8 +9,11 @@
 // vcpkg
 #include "lodepng.h"
 
-constexpr int WIDTH = 640;		// number of pixels on x-axis;
-constexpr int HEIGHT = 360;		// number of pixels on y-axis;
+#define PROGRAM_FILE	"kernel.cl"
+#define KERNEL_FUNC		"apply_shader"
+
+constexpr int WIDTH = 512;		// number of pixels on x-axis;
+constexpr int HEIGHT = 512;		// number of pixels on y-axis;
 constexpr int DEPTH = 24;		// color depth in bits;
 
 #define SIZE		HEIGHT * WIDTH	// total image pixels
@@ -96,7 +99,7 @@ int main()
 
 	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &err);
 
-	cl_program program = build_program(context, device_id, "kernel.cl", err);
+	cl_program program = build_program(context, device_id, PROGRAM_FILE, err);
 
 	// create vector the size of the devices memory
 	std::vector<unsigned char> out(BYTES);
@@ -104,18 +107,33 @@ int main()
 	// create output buffer in which the kernel writes and the host copies out of
 	cl_mem out_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, BYTES, out.data(), &err);
 
+	cl_mem const_width = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(cl_int), NULL, &err);
+	cl_mem const_height = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(cl_int), NULL, &err);
+	cl_mem const_channels = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(cl_int), NULL, &err);
+
 	cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, 0, &err);
 
-	cl_kernel kernel = clCreateKernel(program, "apply_shader", &err);
+	cl_int width = WIDTH;
+	cl_int height = HEIGHT;
+	cl_int channels = CHANNELS;
+	
+	err = clEnqueueWriteBuffer(command_queue, const_width, CL_TRUE, 0, sizeof(cl_int), &width, 0, NULL, NULL);
+	err = clEnqueueWriteBuffer(command_queue, const_height, CL_TRUE, 0, sizeof(cl_int), &height, 0, NULL, NULL);
+	err = clEnqueueWriteBuffer(command_queue, const_channels, CL_TRUE, 0, sizeof(cl_int), &channels, 0, NULL, NULL);
+
+	cl_kernel kernel = clCreateKernel(program, KERNEL_FUNC, &err);
 
 	// set output-buffer as argument (index 0)
 	err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &out_buffer);
+	err = clSetKernelArg(kernel, 1, sizeof(cl_mem), &const_width);
+	err = clSetKernelArg(kernel, 2, sizeof(cl_mem), &const_height);
+	err = clSetKernelArg(kernel, 3, sizeof(cl_mem), &const_channels);
 
-	size_t global_size = BYTES;
-	size_t local_size = 64; // divide work ino blocks of 64
+	size_t global_size[] = { WIDTH, HEIGHT };
+	size_t local_size[] = { 64, 64 }; // divide work ino blocks of 64
 
 	// this executes the kernel code
-	err = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
+	err = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global_size, local_size, 0, NULL, NULL);
 
 	// read device memory into heap memory
 	err = clEnqueueReadBuffer(command_queue, out_buffer, CL_TRUE, 0, BYTES, out.data(), 0, NULL, NULL);
