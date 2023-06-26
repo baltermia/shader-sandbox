132d63c459b5477f5ce5353d43b6833ee352fe31
baltermia <54800864+baltermia@users.noreply.github.com>
2023-06-26 08:55:33 +0200

    Use ConfigureCL namespace in main

--- FileOutput/OpenCL/main.cpp
+++ FileOutput/OpenCL/main.cpp
@@ -9,6 +9,11 @@
 // vcpkg
 #include "lodepng.h"
 
+// lib
+#include "ConfigureCL/ConfigureCL.h"
+
+using namespace ConfigureCL;
+
 #define PROGRAM_FILE	"kernel.cl"
 #define KERNEL_FUNC		"apply_shader"
 
@@ -20,75 +25,6 @@ constexpr int DEPTH = 24;		// color depth in bits;
 #define BITS		SIZE * DEPTH	// total image size in bits
 #define BYTES		BITS / 8		// total image size in bytes
 #define CHANNELS	DEPTH / 8		// number of bytes per color
-
-/// <summary>
-/// Try and create a device. Prefferably GPU over CPU
-/// </summary>
-cl_device_id create_device(cl_int& err)
-{
-	cl_platform_id platform;
-	cl_device_id device_id;
-
-	// Identify platform
-	err = clGetPlatformIDs(1, &platform, NULL);
-
-	// Try to access a GPU
-	err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-
-	// If no GPU could be found, use CUP
-	if (err == CL_DEVICE_NOT_FOUND)
-		err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
-
-	return device_id;
-}
-
-/// <summary>
-/// Create program from a file and compile it
-/// </summary>
-cl_program build_program(cl_context context, cl_device_id device_id, const char* filename, cl_int& err)
-{
-	// Load file
-	std::ifstream file(filename);
-
-	// Seek to end of file.
-	file.seekg(0, std::ios_base::end);
-
-	// get file-size
-	size_t program_size = file.tellg();
-
-	// go to beginning
-	file.seekg(0);
-
-	// read file into memory
-	std::vector<char> program_buffer(program_size);
-	file.read(program_buffer.data(), program_size);
-
-	const char* ptr = program_buffer.data();
-
-	// create new program with buffer
-	cl_program program = clCreateProgramWithSource(context, 1, (const char**)&ptr, &program_size, &err);
-
-	// build program
-	err = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-
-	if (err >= 0)
-		return program;
-
-	// if build is not successful, print the build-info to the console
-
-	size_t log_size;
-	// first get size of log-text
-	clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-
-	// get that memory
-	std::vector<char> program_log(log_size);
-	clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, log_size + 1, program_log.data(), NULL);
-
-	// finally print it to the console
-	printf("%s\n", program_log.data());
-
-	return program;
-}
 
 int main()
 {
